2e3aba2965da7e31e2d11202262d152de50b5249
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-09-26 10:06:19 +0200

    Use async-await and implement basic errorhandling

--- src/controllers/codes.ts
+++ src/controllers/codes.ts
@@ -1,8 +1,11 @@
 import fetch from "node-fetch";
 import { Response, Request } from "express";
+import ErrorResponse from "../models/ErrorResponse.model";
+
+const API_URL = "https://api.trafikinfo.trafikverket.se/v2/data.json";
 
 const codes = {
-    getCodes: async function getCodes(req: Request, res: Response): Promise<void> {
+    getCodes: async function getCodes(req: Request, res: Response): Promise<object | ErrorResponse> {
         const query = `<REQUEST>
                   <LOGIN authenticationkey="${process.env.TRAFIKVERKET_API_KEY}" />
                   <QUERY objecttype="ReasonCode" schemaversion="1">
@@ -13,19 +16,30 @@ const codes = {
                   </QUERY>
             </REQUEST>`;
 
-        const response = fetch("https://api.trafikinfo.trafikverket.se/v2/data.json", {
-            method: "POST",
-            body: query,
-            headers: { "Content-Type": "text/xml" }
-        })
-            .then(function (response: { json: () => any }) {
-                return response.json();
-            })
-            .then(function (result: { RESPONSE: { RESULT: { ReasonCode: any }[] } }) {
-                return res.json({
-                    data: result.RESPONSE.RESULT[0].ReasonCode
-                });
+        try {
+            const response = await fetch(API_URL, {
+                method: "POST",
+                body: query,
+                headers: { "Content-Type": "text/xml" }
             });
+
+            const result = await response.json(); 
+            
+            return res.json({
+                data: result.RESPONSE.RESULT[0].ReasonCode
+            });
+        } catch (err) {
+            console.error(`Error fetching codes: ${err}`);
+
+            res.status(500).json({
+                errors: {
+                    status: 500,
+                    source: API_URL,
+                    title: 'Server Error',
+                    message: err.message
+                }
+            });
+        }
     }
 };
 
