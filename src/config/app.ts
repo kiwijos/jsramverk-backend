e3a1fd2dad9880af1b2db9922af32eed63ad73b9
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-10-04 18:25:21 +0200

    Make graphql handler

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,6 +1,8 @@
 import express, { Express } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { graphqlHTTP } from "express-graphql";
+import { schema, resolver } from "./schema/index";
 
 import delayed from "../routes/delayed";
 import tickets from "../routes/tickets";
@@ -15,6 +17,16 @@ app.disable("x-powered-by");
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Make handler a graphql handler
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        rootValue: resolver,
+        graphiql: true
+    })
+);
 
 app.use("/delayed", delayed);
 app.use("/tickets", tickets);
